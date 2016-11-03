481dbca4d45ba57cab34e07c9002be1e6bfc8af7
Jason Arnold <jason.arnold.00@gmail.com>
2016-11-03 10:38:04 -0400

    Done.

---
+++ arrays.js
@@ -0,0 +1,38 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(arr, element){
+   newArr = [element, ...arr]
+   return newArr
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, element){
+  arr.unshift(element)
+  return arr
+}
+
+function addElementToEndOfArray(arr, element){
+  newArr = [...arr, element]
+  return newArr
+}
+
+function destructivelyAddElementToEndOfArray(arr, element){
+  arr.push(element)
+  return arr
+}
+
+function accessElementInArray(arr, element){
+  return arr[element]
+}
+
+function removeElementFromBeginningOfArray(arr){
+  arr.shift(1)
+  return arr
+}
+
+function removeElementFromEndOfArray(arr){
+  arr.pop()
+  return arr
+}
+
+
+
